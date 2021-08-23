a20fdf9424b8ec80fc482c80ff5c43e9d0b38afc
Wa Ode Zalmawati <66761941+waodezalmawati@users.noreply.github.com>
2021-08-23 14:10:14 +0700

    My First Commit
    Try to get token graph ig

---
+++ defines.php
@@ -0,0 +1,16 @@
+<?php
+	session_start();
+
+	define( 'FACEBOOK_APP_ID', '1298517400604284' );
+	define( 'FACEBOOK_APP_SECRET', '●●●●●●●●' );
+	define( 'FACEBOOK_REDIRECT_URI', 'https://github.com/waodezalmawati/Graph-API-Instagram/blob/main/defines.php' );
+	define( 'ENDPOINT_BASE', 'https://graph.facebook.com/v5.0/' );
+
+	// accessToken
+	$accessToken = 'YOUR-ACCESS-TOKEN-HERE';
+
+	// page id
+	$pageId = 'YOUR-PAGE-ID';
+
+	// instagram business account id
+	$instagramAccountId = 'YOUR-INSTAGRAM-ACCOUNT-ID';
